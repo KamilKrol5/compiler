576c76324047be1a80a4248fbac34662050ce74b
Kamil <kamil55525@gmail.com>
2019-12-23 20:01:02 +0100

    Add printing ast

--- src/structures/AST.py
+++ src/structures/AST.py
@@ -1,12 +1,20 @@
 from typing import List
-from abc import ABC
+from abc import ABC, abstractmethod
+
+INDENT = '   '
 
 
-class Value(ABC):
+class PrintableWithIndent:
+    @abstractmethod
+    def to_str_with_indent(self, indent=0) -> str:
+        pass
+
+
+class Value(ABC, PrintableWithIndent):
     pass
 
 
-class Identifier(ABC):
+class Identifier(ABC, PrintableWithIndent):
     pass
 
 
@@ -14,15 +22,26 @@ class IntNumberValue(Value):
     def __init__(self, value: int):
         self.value = value
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<IntNumberValue[ value = {self.value} ]>'
+
 
 class IdentifierValue(Value):
     def __init__(self, identifier: Identifier):
         self.identifier = identifier
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<IdentifierValue[ ' \
+            f'identifier = \n{self.identifier.to_str_with_indent(indent + 1)}\n' \
+            + indent * INDENT + ']>'
+
 
 class VariableIdentifier(Identifier):
     def __init__(self, identifier_name: str):
         self.identifier_name = identifier_name
+
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<VariableIdentifier[ identifier_name = {self.identifier_name} ]>'
 
 
 class ArrayElementByVariableIdentifier(Identifier):
@@ -30,20 +49,31 @@ class ArrayElementByVariableIdentifier(Identifier):
         self.array_identifier = array_identifier
         self.index_identifier = index_identifier
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<ArrayElementByVariableIdentifier[ array_identifier = {self.array_identifier}, ' \
+            f'index_identifier = {self.index_identifier} ]>'
+
 
 class ArrayElementByIntNumberIdentifier(Identifier):
     def __init__(self, array_identifier: str, index_value: IntNumberValue):
         self.array_identifier = array_identifier
-        self.index_identifier = index_value
+        self.index_value = index_value
+
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<ArrayElementByIntNumberIdentifier[ array_identifier = {self.array_identifier}, ' \
+            f'index_value = \n{self.index_value.to_str_with_indent(indent + 1)}\n]>'
 
 
-class Declaration(ABC):
+class Declaration(ABC, PrintableWithIndent):
     pass
 
 
 class NumberDeclaration(Declaration):
     def __init__(self, identifier: str):
         self.identifier = identifier
+
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<NumberDeclaration[ id = {self.identifier} ]>'
 
 
 class ArrayDeclaration(Declaration):
@@ -52,30 +82,44 @@ class ArrayDeclaration(Declaration):
         self.begin_index = begin_index
         self.identifier = identifier
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<ArrayDeclaration[ id = {self.identifier}, begin_index = ' + \
+               f'\n{self.begin_index.to_str_with_indent(indent + 1)},\n' + \
+               indent * INDENT + f'end_index = \n' + \
+               f'{self.end_index.to_str_with_indent(indent + 1)}\n' + \
+               indent * INDENT + f']>'
 
-class Declarations:
+
+class Declarations(PrintableWithIndent):
     def __init__(self, declarations: List[Declaration]):
         self.declarations = declarations
 
     def add_declaration(self, declaration: Declaration) -> None:
         self.declarations.append(declaration)
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return str(('\n' + (indent * INDENT)).join(map(lambda x: x.to_str_with_indent(indent + 1), self.declarations)))
 
-class Command(ABC):
+
+class Command(ABC, PrintableWithIndent):
     pass
 
 
-class Expression(ABC):
+class Expression(ABC, PrintableWithIndent):
     pass
 
 
-class Condition(ABC):
+class Condition(ABC, PrintableWithIndent):
     pass
 
 
 class ExpressionHavingOnlyOneValue(Expression):
     def __init__(self, value: Value):
         self.value = value
+
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<ExpressionHavingOnlyOneValue[ value = ' \
+            f'\n{self.value.to_str_with_indent(indent + 1)}\n' + indent * INDENT + f']>'
 
 
 class ExpressionHavingTwoValues(Expression):
@@ -84,26 +128,49 @@ class ExpressionHavingTwoValues(Expression):
         self.valueLeft = value1
         self.valueRight = value2
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<ExpressionHavingTwoValues[' \
+               f' value_left = \n{self.valueLeft.to_str_with_indent(indent + 1)}\n' + \
+               indent * INDENT + f'value_right = \n{self.valueRight.to_str_with_indent(indent + 1)}\n' + \
+               indent * INDENT + f'operation = {self.operation}\n' + indent * INDENT + ']>'
+
 
 class TwoValueCondition(Condition):
+
     def __init__(self, value1: Value, value2: Value, compare_operation: str):
         self.compare_operation = compare_operation
         self.valueLeft = value1
         self.valueRight = value2
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<TwoValueCondition[' \
+               f'value_left = \n{self.valueLeft.to_str_with_indent(indent + 1)}\n' + \
+               indent * INDENT + f'value_right = \n{self.valueRight.to_str_with_indent(indent + 1)}\n' + \
+               indent * INDENT + f'operation = {self.compare_operation}\n' + indent * INDENT + ']>'
 
-class Commands:
+
+class Commands(PrintableWithIndent):
     def __init__(self, commands: List[Command]):
         self.commands = commands
 
     def add_command(self, command: Command) -> None:
         self.commands.append(command)
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + str(('\n' + (indent * INDENT))
+                                     .join(map(lambda x: x.to_str_with_indent(indent + 1), self.commands)))
+
 
 class AssignmentCommand(Command):
     def __init__(self, identifier: Identifier, expression: Expression):
         self.expression = expression
         self.identifier = identifier
+
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<AssignmentCommand[ ' \
+               f'identifier = \n{self.identifier.to_str_with_indent(indent + 1)} \n' + \
+               indent * INDENT + f'expression = \n{self.expression.to_str_with_indent(indent + 1)}\n' + \
+               indent * INDENT + f']>'
 
 
 class IfThenElseCommand(Command):
@@ -112,26 +179,60 @@ class IfThenElseCommand(Command):
         self.commands_false = commands1
         self.condition = condition
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<IfThenElseCommand[ condition = \n' \
+            f'{self.condition.to_str_with_indent(indent+1)},\n' +\
+            indent * INDENT + f'commands_true =\n' \
+            f'{self.commands_true.to_str_with_indent(indent+1)}\n' +\
+            indent * INDENT + f'commands_false =\n' \
+            f'{self.commands_false.to_str_with_indent(indent+1)}\n' +\
+            indent * INDENT + f']> '
+
 
 class IfThenCommand(Command):
+
     def __init__(self, condition: Condition, commands: Commands):
         self.commands_true = commands
         self.condition = condition
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<IfThenCommand[ condition = \n' \
+            f'{self.condition.to_str_with_indent(indent + 1)},\n' + \
+            indent * INDENT + f'commands_true =\n' \
+            f'{self.commands_true.to_str_with_indent(indent + 1)}\n' + \
+            indent * INDENT + f']> '
+
 
 class WhileDoCommand(Command):
+
     def __init__(self, condition: Condition, commands: Commands):
         self.commands = commands
         self.condition = condition
+
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<WhileDoCommand[ condition =\n' \
+            f'{self.condition.to_str_with_indent(indent + 1)}\n' + \
+            indent * INDENT + f'commands = \n' \
+            f'{self.commands.to_str_with_indent(indent + 1)}\n' + \
+            indent * INDENT + ']>'
 
 
 class DoWhileCommand(Command):
+
     def __init__(self, condition: Condition, commands: Commands):
         self.commands = commands
         self.condition = condition
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<DoWhileCommand[ condition =\n' \
+            f'{self.condition.to_str_with_indent(indent + 1)}\n' + \
+            indent * INDENT + f'commands = \n' \
+            f'{self.commands.to_str_with_indent(indent + 1)}\n' + \
+            indent * INDENT + ']>'
+
 
 class ForCommand(Command):
+
     def __init__(self, iterator_identifier: str, start: Value, end: Value, is_down_to: bool, commands: Commands):
         self.iterator_identifier = iterator_identifier
         self.start = start
@@ -139,15 +240,33 @@ class ForCommand(Command):
         self.is_down_to = is_down_to
         self.commands = commands
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<ForCommand[ iterator_identifier = {self.iterator_identifier}, ' \
+            f'is_down_to = {self.is_down_to}, start = \n' + \
+            f'{self.start.to_str_with_indent(indent + 1)}\n' + \
+            indent * INDENT + f'end =\n' \
+            f'{self.end.to_str_with_indent(indent + 1)}\n' + \
+            indent * INDENT + f'commands = \n' \
+            f'{self.commands.to_str_with_indent(indent + 1)}\n' + \
+            indent * INDENT + ']>'
+
 
 class ReadCommand(Command):
     def __init__(self, identifier: Identifier):
         self.identifier = identifier
 
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<ReadCommand[ id = \n{self.identifier.to_str_with_indent(indent + 1)}\n' + \
+               indent * INDENT + ']>'
+
 
 class WriteCommand(Command):
     def __init__(self, value: Value):
         self.value = value
+
+    def to_str_with_indent(self, indent=0) -> str:
+        return indent * INDENT + f'<WriteCommand[ value = \n{self.value.to_str_with_indent(indent + 1)}\n' + \
+               indent * INDENT + ']>'
 
 
 class Program:
@@ -155,38 +274,6 @@ class Program:
         self.commands = commands
         self.declarations = declarations
 
-# class TreeNode:
-#     def __init__(self, children: List['TreeNode'], parent: 'TreeNode', child_id=0):
-#         self.children: List['TreeNode'] = children
-#         self.parent: TreeNode = parent
-#         self.child_id: int = child_id
-#
-#
-# class Tree:
-#     def __init__(self, root: TreeNode, name='tree'):
-#         self.name = name
-#         self.root = root
-#
-#     def __str__(self):
-#         return str(self.root)
-#
-#
-# class NumberNode(TreeNode):
-#     def __init__(self, children: List['TreeNode'], parent: 'TreeNode'):
-#         super().__init__(children, parent)
-#
-#
-# class BinaryOperation(TreeNode):
-#     def __init__(self, children: List['TreeNode'], parent: 'TreeNode', operator: str, child_id=0):
-#         super().__init__(children, parent, child_id)
-#         self.operator = operator
-#
-#
-# class WhileNode(TreeNode):
-#     def __init__(self, children: List['TreeNode'], parent: 'TreeNode'):
-#         super().__init__(children, parent)
-#
-#
-# class ANumber:
-#     def __init__(self, value):
-#         self.value = value
+    def __str__(self):
+        return "Declarations:\n" + str(self.declarations.to_str_with_indent(0)) + "\nCommands:\n" + str(
+            self.commands.to_str_with_indent(0))
