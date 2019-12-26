43342361a936550cddb58fa53f65ce16a82f9595
Kamil <kamil55525@gmail.com>
2019-12-26 20:20:05 +0100

    Update arrays utils

--- src/utils/arrays_utils.py
+++ src/utils/arrays_utils.py
@@ -1,4 +1,5 @@
 from structures.AST import *
+from utils.math_utils import generate_number
 from typing import Dict, Tuple
 
 ''' Computes relative register of element of the array based on array declaration.
@@ -16,7 +17,43 @@ def compute_relative_register_of_array_element(declaration: ArrayDeclaration, us
 ''' Computes real register of element of the array based on declared array list.'''
 
 
-def compute_real_register_of_array_element(declared_arrays: Dict[str, Tuple[int, int, ArrayDeclaration]],
-                                           identifier: str, user_typed_element_index: int) -> int:
-    return declared_arrays[identifier][0] + compute_relative_register_of_array_element(declared_arrays[identifier][2],
-                                                                                       user_typed_element_index)
+def compute_real_register_of_array_element(
+        declared_arrays: Dict[str, Tuple[int, int, ArrayDeclaration]],
+        identifier: ArrayElementByIntNumberIdentifier) -> int:
+    return declared_arrays[identifier.array_identifier][0] + compute_relative_register_of_array_element(
+        declared_arrays[identifier.array_identifier][2], identifier.index_value.value)
+
+
+''' Generates code for loading value of array element which is indexed by variable. Load is called.
+    Registers used: 0-5'''
+
+
+def generate_code_for_loading_array_element_by_variable(
+        identifier: ArrayElementByVariableIdentifier,
+        declared_variables: Dict[str, int],
+        declared_arrays: Dict[str, Tuple[int, int, ArrayDeclaration]]
+) -> str:
+    return generate_code_for_computing_index_of_array_element_by_variable(identifier, declared_variables,
+                                                                          declared_arrays) + \
+           f'LOADI 0\n'
+
+
+''' Generates code for computing index of array element which is indexed by variable. 
+    Computed index (result) is present in register 0 (p0).
+    Example: arr(b) is computed from b_value_reg - arr_fictional_indexing_start_register + arr_real_start_register
+    Registers used: 0-5'''
+
+
+# TODO
+# can be improved, numbers which are generated can be generated once when program starts
+# and taken from known registers
+def generate_code_for_computing_index_of_array_element_by_variable(
+        identifier: ArrayElementByVariableIdentifier,
+        declared_variables: Dict[str, int],
+        declared_arrays: Dict[str, Tuple[int, int, ArrayDeclaration]]
+) -> str:
+    return generate_number(declared_arrays[identifier.array_identifier][0], 5) + \
+           generate_number(declared_arrays[identifier.array_identifier][2].begin_index.value, 4) + \
+           f'LOAD {declared_variables[identifier.index_identifier]}\n' +\
+               f'SUB 4\n' +\
+               f'ADD 5\n'
