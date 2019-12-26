249422fc516e0f6428836da7b08ef03ad51e2d13
Kamil <kamil55525@gmail.com>
2019-12-26 18:32:11 +0100

    Add loading values from elements of arrays by declared variable (arr[i])

--- src/utils/loop_utils.py
+++ src/utils/loop_utils.py
@@ -12,59 +12,76 @@ def generate_for_loop(loop: ForCommand) -> str:
 
 
 ''' Loads variable defined in identifier to dest_register (default is 0).
-    If variable name is not present in the declared_variables dictionary an exception is raised'''
+    If variable name is not present in the declared_variables dictionary an exception is raised
+    Registers used: 0-5'''
 
 
 def load_value_by_identifier(identifier: Identifier, declared_variables: Dict[str, int], dest_register=0,
                              declared_arrays: Dict[str, Tuple[int, int, ArrayDeclaration]] = {}) -> str:
-    result: str = ''
+    result: str = f'## load value by identifier begin\n'
     if isinstance(identifier, VariableIdentifier):
         if identifier.identifier_name not in declared_variables.keys():
             raise ValueError('declared_variables dictionary does not contain key - identifier name provided with '
                              '"identifier" argument. Variable might not be declared.')
-        result = f'LOAD {declared_variables[identifier.identifier_name]}\n'
+        result = result + f'LOAD {declared_variables[identifier.identifier_name]}\n'
     elif isinstance(identifier, ArrayElementByIntNumberIdentifier):
         if identifier.array_identifier not in declared_arrays.keys():
+            raise ValueError('declared_arrays dictionary does not contain key - identifier name provided with '
+                             '"identifier" argument. Variable might not be declared.')
+        result = result + f'LOAD {compute_real_register_of_array_element(declared_arrays, identifier.array_identifier, identifier.index_value.value)}\n'
+    elif isinstance(identifier, ArrayElementByVariableIdentifier):
+        if identifier.array_identifier not in declared_arrays.keys():
+            raise ValueError('declared_arrays dictionary does not contain key - identifier name provided with '
+                             '"identifier" argument. Variable might not be declared.')
+        if identifier.index_identifier not in declared_variables.keys():
             raise ValueError('declared_variables dictionary does not contain key - identifier name provided with '
                              '"identifier" argument. Variable might not be declared.')
-        result = f'LOAD {compute_real_register_of_array_element(declared_arrays, identifier.array_identifier, identifier.index_value.value)}\n'
         # TODO
-    elif isinstance(identifier, ArrayElementByVariableIdentifier):
-        pass
-        # TODO
+        # can be improved, numbers which are generated can be generated once when program starts
+        # and taken from known registers
+        result = result + generate_number(declared_arrays[identifier.array_identifier][0], 5) + \
+            generate_number(declared_arrays[identifier.array_identifier][2].begin_index.value, 4) + \
+            f'LOAD {declared_variables[identifier.index_identifier]}\n' \
+            f'SUB 4\n' \
+            f'ADD 5\n' \
+            f'LOAD 0\n'
 
     if dest_register != 0:
         result = result + f'STORE {dest_register}\n'
-    return result
+    return result + f'## load value by identifier end\n'
 
 
 ''' Generates code for subtraction left value from right value
     Registers used: 0-4'''
 
 
-def generate_condition(condition: TwoValueCondition, declared_variables: Dict[str, int]) -> str:
+def generate_condition(condition: TwoValueCondition, declared_variables: Dict[str, int],
+                       declared_arrays: Dict[str, Tuple[int, int, ArrayDeclaration]]) -> str:
     is_left_id_value = isinstance(condition.valueLeft, IdentifierValue)
     is_right_id_value = isinstance(condition.valueRight, IdentifierValue)
     # TODO handle array identifiers
     if not is_left_id_value and not is_right_id_value:
         left: int = condition.valueLeft.value
         right: int = condition.valueRight.value
-        result = generate_number(right, destination_register=4)
-        result = result + generate_number(left, destination_register=0) + 'SUB 4\n'
+        result = generate_number(right, destination_register=3)
+        result = result + generate_number(left, destination_register=0) + 'SUB 3\n'
     elif not is_left_id_value and is_right_id_value:
         left: int = condition.valueLeft.value
         right: VariableIdentifier = condition.valueRight.identifier
-        result = generate_number(left, destination_register=0) + f'SUB {declared_variables.get(right.identifier_name)}'
+        result = generate_number(left, destination_register=0) + \
+            f'SUB {declared_variables.get(right.identifier_name)}\n'
     elif is_left_id_value and not is_right_id_value:
         left: VariableIdentifier = condition.valueLeft.identifier
         right: int = condition.valueRight.value
-        result = generate_number(right, destination_register=4) + \
-            load_value_by_identifier(left, declared_variables, dest_register=0) + 'SUB 4'
+        result = generate_number(right, destination_register=3) + \
+            load_value_by_identifier(left, declared_variables, dest_register=0, declared_arrays=declared_arrays) + \
+            'SUB 3\n'
     elif is_left_id_value and is_right_id_value:
         left: VariableIdentifier = condition.valueLeft.identifier
         right: VariableIdentifier = condition.valueRight.identifier
-        result = load_value_by_identifier(left, declared_variables, dest_register=0) + \
-            f'SUB {declared_variables.get(right.identifier_name)}'
+        result = load_value_by_identifier(left, declared_variables, dest_register=3, declared_arrays=declared_arrays) +\
+            load_value_by_identifier(right, declared_variables, dest_register=0, declared_arrays=declared_arrays) + \
+            f'SUB 3\n'
     else:
         raise ValueError('Unknown instance of Identifier occurred.')
     return result
--- src/utils_tests/loop_utils_test.py
+++ src/utils_tests/loop_utils_test.py
@@ -9,7 +9,19 @@ def test_arr_by_val(i=7):
     res = load_value_by_identifier(arr, {}, 55, {'arr': (120, 135, ArrayDeclaration('arr', IntNumberValue(-5),
                                                                                     IntNumberValue(10)))})
     print(res)
-    assert res == f'LOAD {120+5+i}\nSTORE 55\n'
+    assert res == f'LOAD {120 + 5 + i}\nSTORE 55\n'
+
+
+def test_arr_by_var():
+    print(f'_______array by declared variable_______')
+    arr = ArrayElementByVariableIdentifier('arr', 'b')
+    res = load_value_by_identifier(arr, {'b': 32}, 55, {'arr': (120, 135, ArrayDeclaration('arr', IntNumberValue(-20),
+                                                                                           IntNumberValue(15)))})
+    print(res)
+    assert res == '## load value by identifier begin\nSUB 0\nINC\nSTORE 1\nDEC\nINC\nSHIFT 1\nINC\nSHIFT 1\nINC\n' \
+                  'SHIFT 1\nINC\nSHIFT 1\nSHIFT 1\nSHIFT 1\nSTORE 5\nSUB 0\nINC\nSTORE 1\nDEC\nINC\nSHIFT 1\nSHIFT 1' \
+                  '\nINC\nSHIFT 1\nSHIFT 1\nSTORE 1\nSUB 0\nSUB 1\nSTORE 4\nLOAD 32\nSUB 4\nADD 5\nLOAD 0\nSTORE 55\n' \
+                  '## load value by identifier end\n'
 
 
 def test_val_val():
@@ -44,10 +56,59 @@ def test_var_var():
     print(generate_condition(cond, {"a": 102, "b": 133}))
 
 
+def test_arr_by_var_arr_by_var():
+    print('_______var LE var_______')
+    left = IdentifierValue(ArrayElementByVariableIdentifier('brr', 'c'))
+    right = IdentifierValue(ArrayElementByVariableIdentifier('arr', 'c'))
+    cond = TwoValueCondition(left, right, 'LE')
+    print(generate_condition(cond, {"c": 32, "d": 64},
+                             declared_arrays={'arr': (120, 135, ArrayDeclaration('arr', IntNumberValue(-20),
+                                                                                 IntNumberValue(15))), 'brr': (
+                                 256, 266, ArrayDeclaration('brr', IntNumberValue(-5), IntNumberValue(5)))}))
+
+
+def test_arr_by_var_arr_by_val():
+    print('_______var LE var_______')
+    left = IdentifierValue(ArrayElementByVariableIdentifier('brr', 'c'))
+    right = IdentifierValue(ArrayElementByIntNumberIdentifier('arr', IntNumberValue(11)))
+    cond = TwoValueCondition(left, right, 'LE')
+    print(generate_condition(cond, {"c": 32, "d": 64},
+                             declared_arrays={'arr': (120, 135, ArrayDeclaration('arr', IntNumberValue(-20),
+                                                                                 IntNumberValue(15))), 'brr': (
+                                 256, 266, ArrayDeclaration('brr', IntNumberValue(-5), IntNumberValue(5)))}))
+
+
+def test_arr_by_val_arr_by_var():
+    print('_______var LE var_______')
+    left = IdentifierValue(ArrayElementByIntNumberIdentifier('brr', IntNumberValue(3)))
+    right = IdentifierValue(ArrayElementByVariableIdentifier('arr', 'c'))
+    cond = TwoValueCondition(left, right, 'LE')
+    print(generate_condition(cond, {"c": 32, "d": 64},
+                             declared_arrays={'arr': (120, 135, ArrayDeclaration('arr', IntNumberValue(-20),
+                                                                                 IntNumberValue(15))), 'brr': (
+                                 256, 266, ArrayDeclaration('brr', IntNumberValue(-5), IntNumberValue(5)))}))
+
+
+def test_arr_by_val_arr_by_val():
+    print('_______var LE var_______')
+    left = IdentifierValue(ArrayElementByIntNumberIdentifier('brr', IntNumberValue(3)))
+    right = IdentifierValue(ArrayElementByIntNumberIdentifier('arr', IntNumberValue(7)))
+    cond = TwoValueCondition(left, right, 'LE')
+    print(generate_condition(cond, {"c": 32, "d": 64},
+                             declared_arrays={'arr': (120, 135, ArrayDeclaration('arr', IntNumberValue(-20),
+                                                                                 IntNumberValue(15))), 'brr': (
+                                 256, 266, ArrayDeclaration('brr', IntNumberValue(-5), IntNumberValue(5)))}))
+
+
 if __name__ == '__main__':
-    test_val_val()
-    test_var_val()
-    test_val_var()
-    test_var_var()
-    test_arr_by_val()
-    test_arr_by_val(0)
+    # test_val_val()
+    # test_var_val()
+    # test_val_var()
+    # test_var_var()
+    # test_arr_by_val()
+    # test_arr_by_val(0)
+    # test_arr_by_var()
+    # test_arr_by_var_arr_by_var()
+    # test_arr_by_val_arr_by_var()
+    # test_arr_by_val_arr_by_val()
+    test_arr_by_var_arr_by_val()
