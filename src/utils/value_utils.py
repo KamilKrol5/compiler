47fcf62c855e08fbd5ad9a0306287241087f68c2
Kamil <kamil55525@gmail.com>
2019-12-27 20:40:04 +0100

    Add computing value register to value_utils

--- src/utils/value_utils.py
+++ src/utils/value_utils.py
@@ -1,5 +1,8 @@
 from utils.arrays_utils import *
 
+''' Generates code for loading numeric value of Value object to the register 0 (p0).
+    Each subclass of Value is supported.'''
+
 
 def generate_code_for_loading_value(
         value: Value,
@@ -30,3 +33,33 @@ def generate_code_for_loading_value(
             raise ValueError('Unknown instance of Identifier occurred.')
     else:
         raise ValueError('Unknown instance of Value occurred.')
+
+
+''' Computes register number where value of provided IdentifierValue is stored.
+    Only VariableIdentifier and ArrayElementByIntNumberIdentifier is supported,
+    because computation of register for ArrayElementByVariableIdentifier requires generating additional assembly code.
+    If IdentifierValue having ArrayElementByVariableIdentifier as identifier is provided, then ValueError is raised.
+    The purpose of this function is to provide an ability to perform computations on values without the need
+    of loading to the register 0 (p0)'''
+
+
+def compute_value_register(
+        value: IdentifierValue,
+        declared_variables: Dict[str, int],
+        declared_arrays: Dict[str, Tuple[int, int, ArrayDeclaration]]
+) -> int:
+    if isinstance(value.identifier, VariableIdentifier):
+        if value.identifier.identifier_name not in declared_variables.keys():
+            ValueError('declared_variables dictionary does not contain key - identifier name provided with '
+                       'write_command`s property - identifier. Variable might not be declared.')
+        return declared_variables[value.identifier.identifier_name]
+    elif isinstance(value.identifier, ArrayElementByIntNumberIdentifier):
+        if value.identifier.array_identifier not in declared_arrays.keys():
+            ValueError('declared_arrays dictionary does not contain key - identifier name provided with '
+                       'write_command`s property - identifier. Variable might not be declared.')
+        return compute_real_register_of_array_element(declared_arrays, value.identifier)
+    elif isinstance(value.identifier, ArrayElementByVariableIdentifier):
+        raise ValueError('Cannot compute register of value which identifier is ArrayElementByVariableIdentifier.'
+                         'Separate assembly code needs to be created to do so.')
+    else:
+        raise ValueError('Unknown instance of Identifier occurred.')
