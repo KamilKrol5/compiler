7e58786b0ac78c57f6d99b5385c34989eaddaaad
Kamil <kamil55525@gmail.com>
2020-01-14 03:07:09 +0100

    Optimize condition generating

--- src/structures/ast/identifier_register_representation.py
+++ src/structures/ast/identifier_register_representation.py
@@ -10,11 +10,18 @@ class AbstractIdentifierAccess(ABC):
     def prepare_register(self):
         pass
 
+    @abstractmethod
+    def load(self):
+        pass
+
 
 class StaticIdentifierAccess(AbstractIdentifierAccess):
 
     def __init__(self, value: int):
         self.value = value
+
+    def load(self):
+        return f'LOAD {self.value}\n'
 
     def store(self) -> str:
         return f'STORE {self.value}\n'
@@ -27,6 +34,9 @@ class DynamicIdentifierAccess(AbstractIdentifierAccess):
 
     def __init__(self, generating_str: str):
         self.generating_str = generating_str
+
+    def load(self):
+        return f'LOADI {9}\n'
 
     def store(self) -> str:
         return "STOREI 9\n"
--- src/utils/loop_utils.py
+++ src/utils/loop_utils.py
@@ -13,19 +13,28 @@ def generate_condition(condition: TwoValueCondition, visitor: 'ASTInterpreter') 
     if not is_left_id_value and not is_right_id_value:
         left: int = condition.valueLeft.value
         right: int = condition.valueRight.value
-        result = generate_number(right, constants=visitor.constants, destination_register=3)
-        result = result + generate_number(left, constants=visitor.constants, destination_register=0) + 'SUB 3\n'
+        result = generate_number(right - left, constants=visitor.constants, destination_register=0)
     elif not is_left_id_value and is_right_id_value:
         left: int = condition.valueLeft.value
         right: VariableIdentifier = condition.valueRight.identifier
-        result = generate_number(left, constants=visitor.constants, destination_register=0) + \
-            f'SUB {visitor.declared_variables.get(right.identifier_name)}\n'
+        right_access: AbstractIdentifierAccess = right.accept(visitor)
+
+        if left in visitor.constants:
+            result = right_access.prepare_register() + right_access.load() + f'SUB {visitor.constants[left]}\n'
+        else:
+            result = generate_number(left, constants=visitor.constants, destination_register=0) + \
+                f'SUB {visitor.declared_variables.get(right.identifier_name)}\n'
     elif is_left_id_value and not is_right_id_value:
         left: VariableIdentifier = condition.valueLeft.identifier
         right: int = condition.valueRight.value
-        result = generate_number(right, constants=visitor.constants, destination_register=3) + \
-            load_value_by_identifier(left, visitor, dest_register=0) + \
-            'SUB 3\n'
+        left_access: AbstractIdentifierAccess = left.accept(visitor)
+
+        if right in visitor.constants:
+            result = left_access.prepare_register() + left_access.load() + f'SUB {visitor.constants[right]}\n'
+        else:
+            result = generate_number(right, constants=visitor.constants, destination_register=3) + \
+                left_access.prepare_register() + left_access.load() + \
+                'SUB 3\n'
     elif is_left_id_value and is_right_id_value:
         left: VariableIdentifier = condition.valueLeft.identifier
         right: VariableIdentifier = condition.valueRight.identifier
