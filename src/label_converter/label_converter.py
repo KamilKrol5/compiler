2001c8dc3f9bb0c63b7b2ce06db3a9935119e924
Kamil <kamil55525@gmail.com>
2020-01-27 14:40:58 +0100

    Add optional saving intermediate code

--- src/label_converter/label_converter.py
+++ src/label_converter/label_converter.py
@@ -1,52 +1,49 @@
 import re
 
+from utils.utils import write_to_file
 
-def convert_labels_to_registers(filename='filename.txt', output_filename='imp_executable'):
+
+def convert_labels_to_registers(intermediate_code_with_labels: str, output_filename='unnamed_executable',
+                                save_intermediate_code=False):
 
     labels_dest = dict()
     labels_to_replace = dict()
 
-    with open(filename, 'r+') as file:
-        data: str = ''
-        line_counter = 0
-        lines = file.readlines()
-        for line in lines:
-            if re.match(r'^(##).*\n', line):
-                continue
-            if re.match(r'^\n', line):
-                continue
-            if line.startswith('%label_'):
-                labels_dest[line[1:-1]] = line_counter
-            else:
-                line_counter = line_counter + 1
-                if re.match(r'.+%label_.*\n', line):
-                    r = re.search(r'.+%(label_[0-9]+).*\n', line)
-                    if r:
-                        lbl = r.group(1)
-                        labels_to_replace[lbl] = line_counter
-                    else:
-                        raise Exception('No valid label in line.')
-                    #print(line)
-                data = data + line
-
-        for label, reg in labels_dest.items():
-            regex = rf'%{label}$'
-            data = re.sub(regex, str(reg), data, flags=re.MULTILINE)
-            labels_to_replace[label] = None
-
-        if all(map(lambda x: x is None, labels_to_replace.values())):
-            print("Label converter info: All labels, found as needing to be replaced, were replace successfully")
+    if save_intermediate_code:
+        write_to_file(output_filename + '.intermediate', intermediate_code_with_labels)
+    lines = intermediate_code_with_labels.splitlines(keepends=True)
+    data: str = ''
+    line_counter = 0
+    for line in lines:
+        if re.match(r'^(##).*\n', line):
+            continue
+        if re.match(r'^\n', line):
+            continue
+        if line.startswith('%label_'):
+            labels_dest[line[1:-1]] = line_counter
         else:
-            raise Exception(f'Not all labels were replaced! Labels left: '
-                            f'{list(filter(lambda x: labels_to_replace[x] is not None, labels_to_replace.keys()))}')
+            line_counter = line_counter + 1
+            if re.match(r'.+%label_.*\n', line):
+                r = re.search(r'.+%(label_[0-9]+).*\n', line)
+                if r:
+                    lbl = r.group(1)
+                    labels_to_replace[lbl] = line_counter
+                else:
+                    raise Exception('No valid label in line.')
+            data = data + line
 
-        data = re.sub(r'^(##).*\n', '', data, flags=re.MULTILINE)
-        print('_____________________________________________________')
-        #print(data)
-        with(open(output_filename, 'w', newline='\n')) as file2:
-            file2.write(data)
+    for label, reg in labels_dest.items():
+        regex = rf'%{label}$'
+        data = re.sub(regex, str(reg), data, flags=re.MULTILINE)
+        labels_to_replace[label] = None
 
+    if all(map(lambda x: x is None, labels_to_replace.values())):
+        print("[[[ INFO ]]]: Label converter: All labels, found as needing to be replaced, were replaced successfully.")
+    else:
+        raise Exception(f'Not all labels were replaced! Labels left: '
+                        f'{list(filter(lambda x: labels_to_replace[x] is not None, labels_to_replace.keys()))}')
 
-if __name__ == '__main__':
-    convert_labels_to_registers('command_test.txt', 'exe_command_test.txt')
-    convert_labels_to_registers('command_test_all.txt', 'exe_command_test_all.txt')
+    data = re.sub(r'^(##).*\n', '', data, flags=re.MULTILINE)
+    # print('_____________________________________________________')
+    with(open(output_filename, 'w', newline='\n')) as file2:
+        file2.write(data)
